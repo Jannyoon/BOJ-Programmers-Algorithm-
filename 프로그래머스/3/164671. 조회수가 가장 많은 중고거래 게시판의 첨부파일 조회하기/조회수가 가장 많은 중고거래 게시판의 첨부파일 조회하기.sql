6a18f86f78347e31c6a9ee1616011ed136ba6b3e
Jannyoon <149743716+Jannyoon@users.noreply.github.com>
2024-02-10 20:59:54 +0900

    [level 3] Title: 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -0,0 +1,20 @@
+-- USED_GOODS_BOARD와 USED_GOODS_FILE 테이블에서 
+-- 조회수가 가장 높은 중고거래 게시물에 대한 첨부파일 경로를 조회하는 SQL문을 작성해주세요. 
+-- 첨부파일 경로는 FILE ID를 기준으로 내림차순 정렬해주세요. 
+-- 기본적인 파일경로는 /home/grep/src/ 이며, 게시글 ID를 기준으로 디렉토리가 구분되고, 파일이름은 파일 ID, 파일 이름, 파일 확장자로 구성되도록 출력해주세요. 조회수가 가장 높은 게시물은 하나만 존재합니다.
+
+
+-- 코드를 입력하세요
+SELECT CONCAT('/home/grep/src/',A.BOARD_ID,'/',B.FILE_ID,B.FILE_NAME,B.FILE_EXT) AS FILE_PATH
+FROM USED_GOODS_BOARD AS A
+JOIN USED_GOODS_FILE AS B
+ON A.BOARD_ID = B.BOARD_ID
+WHERE VIEWS IN 
+(
+    SELECT MAX(VIEWS) AS VIEWS
+    FROM USED_GOODS_BOARD AS A
+    JOIN USED_GOODS_FILE AS B
+    ON A.BOARD_ID = B.BOARD_ID
+)
+ORDER BY B.FILE_ID DESC;
+    
