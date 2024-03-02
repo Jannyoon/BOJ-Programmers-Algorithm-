805241156f9c2d3e73e2c0a01a61a429bf93cb4c
Jannyoon <149743716+Jannyoon@users.noreply.github.com>
2024-03-02 14:22:20 +0900

    [Gold III] Title: 파일 합치기, Time: 1864 ms, Memory: 33264 KB -BaekjoonHub

--- 백준/Gold/11066. 파일 합치기/파일 합치기.js
+++ 백준/Gold/11066. 파일 합치기/파일 합치기.js
@@ -2,37 +2,37 @@ let fs = require('fs');
 let [tc, ...input] = fs.readFileSync('/dev/stdin').toString().trim()
 .split("\n").map((v)=>v.split(" ").map(Number));
 
-
+let answer = [];
 tc = tc[0];
 let idx = 0;
-let answer = [];
+
 while(idx<input.length){
   let num = input[idx++][0];
-  let files = input[idx++];
-  let minCost = Array.from({length:(num+1)},()=>Array.from({length:(num+1)},()=>0));
+  let list = input[idx++];
+  list.unshift(0);
 
-  let sublist = Array(num+1).fill(0);
-  files.unshift(0);
+  let subList = Array.from({length:(num+1)},()=>0); //해당 사이즈까지 만들어졌을 때 생기는 최종 비용
+  
   for (let i=1; i<=num; i++){
-    sublist[i] = sublist[i-1]+files[i];
+    subList[i] = subList[i-1]+list[i];
   }
 
+
+  let dp = Array.from({length:(num+1)},()=>Array.from({length:(num+1)},()=>0));
+  
   for (let size=1; size<num; size++){
-    for (let start=0; start<=(num); start++){
+    for (let start=0; start<=num; start++){
       let end = start+size;
       if (end>num) break;
-      //console.log(end);
       let result = Infinity;
-      for (let cut = start; cut<end; cut++){
-        result = Math.min(result, minCost[start][cut]+minCost[cut+1][end]+
-          sublist[end]-sublist[start-1]);
-      }
-      minCost[start][end] = result;
 
+      for (let cut=start; cut<end; cut++){
+        result = Math.min(result, dp[start][cut] + dp[cut+1][end] + subList[end]-subList[start-1]);
+      }
+      dp[start][end] = result;
     }
   }
-  answer.push(minCost[1][num]);
-  
+  answer.push(dp[1][num]);
 }
 
-console.log(answer.join('\n'));
+console.log(answer.join("\n"));
