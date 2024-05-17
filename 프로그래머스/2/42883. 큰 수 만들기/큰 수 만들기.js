54b0fa801a9f32c8b4a7f787a42d71c508e2fe68
Jannyoon <149743716+Jannyoon@users.noreply.github.com>
2024-05-17 15:29:32 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.js
@@ -0,0 +1,22 @@
+function solution(number, k) {
+    let stack = [];
+    
+    for (let i=0; i<number.length; i++){
+        let now = number[i];
+        
+        while(k>0 && stack.length!==0 && stack.at(-1)<now){
+            stack.pop();
+            k--;
+        }
+        
+        stack.push(now);
+    }
+    
+    while(k>0){
+        stack.pop();
+        k--;
+    }
+    
+    return stack.join("")
+    
+}
