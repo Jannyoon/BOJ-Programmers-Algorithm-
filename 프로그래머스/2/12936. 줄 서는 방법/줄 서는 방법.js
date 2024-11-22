d033302c4933aa0fd31a649c8f087173cf3748da
Jannyoon <149743716+Jannyoon@users.noreply.github.com>
2024-11-22 22:12:54 +0900

    [level 2] Title: 줄 서는 방법, Time: 0.23 ms, Memory: 32.9 MB -BaekjoonHub

--- 프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.js
+++ 프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.js
@@ -1,36 +1,27 @@
 function solution(n, k) {
-    let arr = Array.from({length:n},(_,i)=>i+1);
-    let result = [];
+    var answer = [];
+    let list = Array.from({length:n},(_,i)=>i+1);
+    let total = factorial(n);
     let nth = k-1;
-  
-    while(arr.length>0){
+    while(list.length>0){
         if (nth===0){
-            result.push(...arr);
+            answer.push(...list);
             break;
         }
-        let factorial = fact(arr.length-1);
-        let idx = nth/factorial >>0; 
-        nth = nth%factorial;
+        let fact = factorial(list.length-1);
+        let idx = Math.floor(nth/fact);
+        nth = nth%fact;
         
-        result.push(arr[idx]);
-        arr.splice(idx,1);
+        answer.push(list[idx]);
+        list.splice(idx,1);
     }
-    
-    return result;
+    return answer;
 }
 
-function fact(n){
+function factorial(num){
     let res = 1;
-    for (let i=2; i<=n; i++){
-        res*=i;
+    for (let i=2; i<=num; i++){
+        res *= i;
     }
     return res;
 }
-/*
-n명의 사람이 일렬로 줄을 서고 있음.
-!!n명의 사람들에겐 각각 1~n번까지 번호가 매겨져 있음.!!
-n명이 사람을 줄을 서는 방법은 여러 가지.
-예를 들어서 3명의 사람이 있다면... 다음과 같이 6개의 방법이 있음
-사람의 수 n과 자연수 k개 주어질 때, 사람을 나열하는 방법을 사전 순으로 나열했을 때
-k번째 방법을 return하는 solution 함수를 완성해주세요
-*/
