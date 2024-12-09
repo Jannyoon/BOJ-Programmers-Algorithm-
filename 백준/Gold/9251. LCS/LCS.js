844051ecd1cd0f8e89ddf06921774bf5de3c5aaa
Jannyoon <149743716+Jannyoon@users.noreply.github.com>
2024-12-10 00:44:07 +0900

    [Gold V] Title: LCS, Time: 284 ms, Memory: 25012 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.js
+++ 백준/Gold/9251. LCS/LCS.js
@@ -3,14 +3,15 @@ let [str1, str2] = fs.readFileSync('/dev/stdin').toString().trim().split("\n");
 
 let len1 = str1.length;
 let len2 = str2.length;
+
+let dp = Array.from({length:len1+1},()=>Array.from({length:len2+1},()=>0));
 let result = 0;
-let dp = Array.from({length:len1+1}, (v)=>Array.from({length:len2+1}, (s)=>0));
 
 for (let i=1; i<=len1; i++){
   for (let j=1; j<=len2; j++){
-    if (str1[i-1]==str2[j-1]) dp[i][j]=dp[i-1][j-1]+1;
-    else dp[i][j]=Math.max(dp[i-1][j], dp[i][j-1]);
-    result = Math.max(result, dp[i][j]);
+    if (str1[i-1]===str2[j-1]) dp[i][j] = dp[i-1][j-1]+1;
+    else dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+    result = Math.max(dp[i][j], result);
   }
 }
 
