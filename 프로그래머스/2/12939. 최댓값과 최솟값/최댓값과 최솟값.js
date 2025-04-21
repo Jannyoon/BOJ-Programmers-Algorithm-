9012ab09ed2c77b28cf42bcc9353e57fe3baa115
Jannyoon <149743716+Jannyoon@users.noreply.github.com>
2025-04-21 20:26:20 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
@@ -1,5 +1,8 @@
-function solution(s) {    
-    let str = s.split(" ").map(Number).sort((a,b)=>a-b);
-    let answer = [str[0], str.at(-1)];
-    return answer.join(" ");
+function solution(s) {
+    let arr = s.split(" ").map(Number);
+    const min = Math.min(...arr);
+    const max = Math.max(...arr);
+    return `${min} ${max}`;
 }
+
+//최소값, 최대값 형태의 문자열을 반환하는 함수.
