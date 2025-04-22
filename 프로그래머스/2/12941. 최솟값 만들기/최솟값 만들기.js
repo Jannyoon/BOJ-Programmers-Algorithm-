e00f0ca727b4cda0e1242f244f8ab1640aa2a696
Jannyoon <149743716+Jannyoon@users.noreply.github.com>
2025-04-22 18:17:04 +0900

    [level 2] Title: 최솟값 만들기, Time: 1.06 ms, Memory: 33 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.js
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.js
@@ -1,16 +1,23 @@
 function solution(A,B){
-    A = A.sort((a,b)=>a-b);
-    B = B.sort((a,b)=>b-a);
+    //둘다 오름차순으로 재정렬
+    A.sort((a,b)=>a-b);    //1 2 3
+    B.sort((a,b)=>a-b);   // 4 5 6
     
-    let result = A.map((v,i)=>v*B[i]).reduce((prev,cur)=>prev+cur,0);
-    console.log(result);
-    return result;
+    let result1 = 0;
+    let result2 = 0;
+    
+    for (let i=0; i<A.length; i++){
+        result1 += A[i]*B[B.length-1-i];
+        result2 += B[i]*A[A.length-1-i];
+    }
+    
+    return Math.min(result1, result2);
 }
 
 /*
-길이가 같은 배열 a,b 두 개가 있습니다. 각 배열은 자연수로 이루어져 있고..
-배열 a,b에서 각각 한 개의 숫자를 뽑아 두 수를 곱합니다.
-이러한 과정을 배열의 길이만큼 반복하며, 두 수를 곱한 값을 누적하여 더합니다.
-이때 최종적으로 누적된 값이 최소가 되도록 만드는 것이 목표입니다 => 그리디? 디피?
-단, 각 배열에서 k번째 숫자를 뽑았다면 다음에 k번째 숫자는 다시 뽑을 수 없습니다.
+길이가 같은 배열 a,b
+각 배열은 자연수로 이루어져 있다.
+
+배열 a,b에서 각각 한 개의 숫자를 뽑아 두 수를 곱한다.
+배열의 길이만큼 반복. 
 */
