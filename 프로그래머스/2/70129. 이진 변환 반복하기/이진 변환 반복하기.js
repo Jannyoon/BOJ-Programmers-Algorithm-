05c6a0f456b619c0640645b5d5774793fcfc8fbf
Jannyoon <149743716+Jannyoon@users.noreply.github.com>
2025-04-22 18:30:25 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 6.69 ms, Memory: 38.2 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
@@ -1,11 +1,17 @@
-function solution(s) {
+function solution(s) {    
     let count = 0;
-    let zeroNum = 0;
+    let zeroes = 0;
+    
+    //s가 1이 될 때까지 계속해서 s에 이진 변환을 가하라
     while(s!=='1'){
-        zeroNum += (s.split("").filter(v => v==='0').length);
-        s = s.split("").filter(v => v!=='0').length.toString(2);
+        let arr = s.split("").filter(v => v!=='0');
+        zeroes += s.length-arr.length; //삭제된 0의 개수 더하기
+        
+        let c = arr.length;
+        s = c.toString(2);
+        
         count++;
     }
     
-    return [count, zeroNum]
+    return [count, zeroes];
 }
