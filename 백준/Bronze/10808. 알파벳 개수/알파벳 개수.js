90a56b9f4f707bca4db896ccb0b75626f432ce65
Jannyoon <149743716+Jannyoon@users.noreply.github.com>
2025-12-28 20:43:24 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 100 ms, Memory: 9352 KB -BaekjoonHub

--- 백준/Bronze/10808. 알파벳 개수/알파벳 개수.js
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.js
@@ -1,12 +1,38 @@
 let fs = require('fs');
 let input = fs.readFileSync('/dev/stdin').toString().trim();
-let str = 'a'
-const defaultNum = str.charCodeAt()
-const alphaList = 'abcdefghijklmnopqrstuvwxyz'
-const alphaArr = Array.from({length:alphaList.length}, () => 0)
+
+let obj = {
+    a: 0,
+    b: 0,
+    c: 0,
+    d: 0,
+    e: 0,
+    f: 0,
+    g: 0,
+    h: 0,
+    i: 0,
+    j: 0,
+    k: 0,
+    l: 0,
+    m: 0,
+    n: 0,
+    o: 0,
+    p: 0,
+    q: 0,
+    r: 0,
+    s: 0,
+    t: 0,
+    u: 0,
+    v: 0,
+    w: 0,
+    x: 0,
+    y: 0,
+    z: 0
+}
 
 for (let i=0; i<input.length; i++){
-    let alpha = input[i]
-    alphaArr[alpha.charCodeAt()-defaultNum]++
+    let key = input[i]
+    obj[key] = obj[key]+1
 }
-console.log(alphaArr.join(" "))
+
+console.log(Object.values(obj).join(" "))
