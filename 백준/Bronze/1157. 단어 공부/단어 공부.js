6596dd0dd9f9a7ac2305fb5ae6e519f3298f9356
Jannyoon <149743716+Jannyoon@users.noreply.github.com>
2026-02-01 19:27:05 +0900

    [Bronze I] Title: 단어 공부, Time: 176 ms, Memory: 12756 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.js
+++ 백준/Bronze/1157. 단어 공부/단어 공부.js
@@ -1,23 +1,19 @@
 let fs = require('fs');
 let input = fs.readFileSync('/dev/stdin').toString().trim().toUpperCase();
 
-let alpha = 'abcdefghijklmnopqrstuvwxyz'.toUpperCase();
-let alphaArr = Array.from({length:alpha.length}, () => 0);
-
-let maxVal = 0;
-let maxIdx = 0;
+let alpha = 'abcdefghijklmnopqrstuvwxyz'.toUpperCase()
+let alphaArr = Array.from({length:alpha.length}, v => 0);
 
 for (let i=0; i<input.length; i++){
-    let nowStr = input[i];
-    let aIdx = alpha.indexOf(nowStr);
+    let str = input[i];
+    let aIdx = alpha.indexOf(str);
     alphaArr[aIdx]++;
-
 }
 
-maxVal = Math.max(...alphaArr)
-let filterList = alphaArr.filter(v => v===maxVal)
-if (filterList.length>1) console.log("?")
+let max = Math.max(...alphaArr)
+let filterArr = alphaArr.filter(v => v===max);
+if (filterArr.length>1) console.log("?");
 else {
-    let idx = alphaArr.findIndex(v => v === maxVal)
+    let idx = alphaArr.findIndex(v => v === max)
     console.log(alpha[idx])
 }
